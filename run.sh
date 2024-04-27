bb059e3b4265d900edbd91b72255bdcccbb8a82c
1337quota <165533514+1337quota@users.noreply.github.com>
2024-04-27 07:10:14 +0000

    fix apt install rclone

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 sudo apt update&&sudo apt -y upgrade
-sudo apt -y install  rclone
+sudo apt -y install rclone
 
 rclone mount --vfs-cache-mode writes --daemon --config rclone.conf onedrive:mcserver $(pwd)/mcserver
 
